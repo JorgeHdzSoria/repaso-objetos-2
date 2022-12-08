a072b1995825f1ef63f1bd7b33f2188ce206e8ab
JorgeHdzSoria <89080013+JorgeHdzSoria@users.noreply.github.com>
2022-12-08 00:32:59 -0600

    Exercise4_1 hora y 8 minutos

---
+++ src/main/java/uaslp/objetos/figuras/escuela/SalaJuntasB.java
@@ -0,0 +1,15 @@
+package uaslp.objetos.escuela;
+
+public class SalaJuntasB implements SalaDeJuntas{
+
+    @Override
+    public String getNombre() {
+        return "Sala B";
+    }
+
+    private static SalaJuntasB instance = new SalaJuntasB();
+
+    public static SalaDeJuntas getInstance() {
+        return instance;
+    }
+}
